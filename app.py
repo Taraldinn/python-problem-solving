5facf184c899418b4cead8792a8f53371dfd1d55
Quazi Md Aldin Fardin <59772487+programmer-Quazi@users.noreply.github.com>
2022-01-21 01:57:12 +0600

    hi all

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("hello world")
+d = {}
+for i in range(int(input())):
+        name = input()        
+        score = float(input())        
+        d.update({name:score})
+        
+print(d)
+s = sorted(d.items(),key=lambda x:x[1])
+print(s)
+
+        
+        
---
+++ dictionary_comprehensions.py
@@ -0,0 +1,8 @@
+values = []
+for x in range(5):
+    values.append(x * 2)
+    
+   
+    
+values = {x: x * 2 for x in range(5)}
+print(values) 
