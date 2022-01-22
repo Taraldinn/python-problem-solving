132900a279ee975b9111d0882c9c31f1732b87e7
programmer-Quazi <kferdoush617@gmail.com>
2022-01-23 03:30:22 +0600

    ok done

--- ganertator_expression.py
+++ ganertator_expression.py
@@ -1,17 +1,18 @@
 class Car:
-    name = ""
-    color = ""
-
-    def __init__(self,n,c):
+    def __init__(self, n , c):
         self.name = n
         self.color = c
 
     def start(self):
-        print("starting the engine")
+        print("name: ", self.name)
+        print("color: ",self.color)
+        print("Starting the engine")
 
 
-my_car = Car("Corolla","White")
-print(my_car.name)
-print(my_car.color)
-my_car.start()
 
+my_car1 =Car("Corolla", "White")
+my_car1.start()
+my_car2 = Car("premio","red")
+my_car2.start()
+my_car3 = Car("BMW", "Blue")
+Car.start(my_car3)
