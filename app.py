ac171039def652e16f8bb638f62754dc589652a7
programmer-Quazi <kferdoush617@gmail.com>
2022-01-23 23:53:31 +0600

    all done

--- app.py
+++ app.py
@@ -1,12 +1,67 @@
-d = {}
-for i in range(int(input())):
-        name = input()        
-        score = float(input())        
-        d.update({name:score})
-        
-print(d)
-s = sorted(d.items(),key=lambda x:x[1])
-print(s)
+class Monster:
+    def __init__(self, color, heads):
+        self.color = color
+        self.heads = heads
 
-        
-        
+
+# Create some real monster
+fogthing = Monster("Black", 5)
+mournsnake = Monster("Yellow", 10)
+tangleface = Monster("white", 15)
+
+# Check whether those monsters got different existence in memory or not
+print('I have ' + str(fogthing.heads) + ' heads and I\'m ' + fogthing.color)
+print('I also have ' + str(mournsnake.heads) + ' heads and I\'m ' + mournsnake.color)
+print('I got ' + str(tangleface.heads) + ' heads and I\'m ' + tangleface.color)
+
+
+# init method
+class Monster:
+    def __init__(self, color, heads):
+        self.color = color
+        self.heads = heads
+
+    @staticmethod
+    def attack():
+        print("just attacked a hero , Boyyyyyyyyyyyyahhhhhhhh!!!")
+
+
+mournsnake = Monster("Yellow", 4)
+print('i am a ' + str(mournsnake.heads) + ' ' 'headed monsters')
+mournsnake.attack()
+
+
+# inheritance
+
+
+class Monster:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def attack(self):
+        print('I am attacking...')
+        print('hey my name is' + str(self.name) + 'and my color is ' + str(self.color))
+
+
+class Fogthing(Monster):
+
+    @staticmethod
+    def make_sound():
+        print('Grrrrrrr\n')
+
+
+class Mournsnake(Monster):
+
+    @staticmethod
+    def make_sound():
+        print('Hiiiissssshhhhh\n')
+
+
+fogthing = Fogthing("Fogthing", "Yellow")
+fogthing.attack()
+fogthing.make_sound()
+
+mournsnake = Mournsnake("Mournsnake", "Red")
+mournsnake.attack()
+mournsnake.make_sound()
--- dictionary_comprehensions.py
+++ dictionary_comprehensions.py
@@ -1,8 +1,6 @@
 values = []
 for x in range(5):
     values.append(x * 2)
-    
-   
-    
+
 values = {x: x * 2 for x in range(5)}
-print(values) 
+print(values)
